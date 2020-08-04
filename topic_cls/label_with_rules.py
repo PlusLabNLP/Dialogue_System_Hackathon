a27575bf2105cf0860a65e2472de1031de3332ab
Zixi Liu <zixiliu@pluslab.isi.edu>
2020-08-04 10:47:59 -0700

    updata label_with_rules.py

--- topic_cls/label_with_rules.py
+++ topic_cls/label_with_rules.py
@@ -6,7 +6,7 @@ import collections
 
 lookup = { i[1] : i[2] for i in csv.reader(open('./entity_topic_assignment.csv', 'rt')) }
 
-def rule_1(data, row):
+def rule_1(data, row, all_labels):
     labels = []
     for k, e in zip(json.loads(row['knowledge_sources']), json.loads(row['entities'])):
         if k.startswith('FS'):
@@ -14,26 +14,48 @@ def rule_1(data, row):
 
     return list(set(labels))
 
-rules = [ rule_1 ]
+def rule_2(data,row, all_labels):
+    try:
+        k1 = int(row['i'])-1
+        rulename, prev_topic = all_labels[(row['conv_id'],k1)]
+    except:
+        prev_topic = None
+    try:
+        k2 = int(row['i'])+1
+        rulename,next_topic = all_labels[(row['conv_id'], k2)]
+    except:
+        next_topic =None
+    if not (prev_topic and next_topic):
+        return []
+    elif not prev_topic and next_topic:
+        return next_topic
+    elif prev_topic and not next_topic:
+        return prev_topic
+    else:
+        return(list(set(prev_topic) & set(next_topic)))
+       
+#def rule_3(data,row,)
+
+
+
+   
+
+rules = [ rule_1 , rule_2]
 
 reader = csv.reader(open('./data/flat_data.tsv', 'rt'), delimiter='\t')
 data = { (row[0], row[1]) : row for row in reader }
+all_labels = {i:('none',None) for i in data.keys()}
+for i, rule in enumerate(rules,1):
+    reader = csv.reader(open('./data/flat_data.tsv', 'rt'), delimiter='\t')
+    header = next(reader)
+    for row in reader:
+        row = { i: j for i, j in zip(header, row) }
+        rule_name = 'rule_%d' % i
+        labels = rule(data, row,all_labels)
+        if (len(labels))>0:
+            all_labels[(row['conv_id'],row['i'])] = (rule_name ,labels)
 
 writer = csv.writer(open('./data/labels.tsv', 'wt'), delimiter='\t')
-reader = csv.reader(open('./data/flat_data.tsv', 'rt'), delimiter='\t')
-header = next(reader)
-for row in reader:
-    row = { i: j for i, j in zip(header, row) }
-
-    for i, rule in enumerate(rules):
-        rule_name = 'rule_%d' % i
-        labels = rule(data, row)
-        
-        if len(labels) > 0:
-            break
-
-    if len(labels) == 0:
-        rule_name = 'none'
-        labels = []
-
-    writer.writerow((row['conv_id'], row['i'], rule_name, json.dumps(labels)))
+for (conv_id, idx), (rulename, labels) in all_labels.items():
+    #row = { i: j for i, j in zip(header, row)  }
+    writer.writerow((conv_id, idx, rulename, json.dumps(labels)))
